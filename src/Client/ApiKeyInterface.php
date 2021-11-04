2b661325edbf66cdc9d34a1de43ae3a058f8d31d
Madalin Ignisca <git@madalin.me>
2021-11-04 11:53:13 +0100

    Create ApiKeyInterface.php

---
+++ src/Client/ApiKeyInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © 2019 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ */
+
+namespace MultiSafepay\Client;
+
+class ApiKeyInterface
+{
+
+    /**
+     * @return string
+     */
+    public function get(): string;
+}
